c4c3fcbb34f81e98c5e7220f8ebb152fc8dc92a6
Harsh selarka <harshselarka2@gmail.com>
2025-04-07 16:50:16 +0530

    constant practice

---
+++ constant.php
@@ -0,0 +1,25 @@
+<?php
+// define("SITE_TITLE","my cool website");
+// echo SITE_TITLE;
+
+
+
+//  class Appconfig{
+//     const APPNAME="ABC";
+//     const VERSION=5.0;
+//  }
+//    $app = new Appconfig();
+//  echo "app name is ".$app::APPNAME."<br>";
+//  echo "app version is ".Appconfig::VERSION."<br>";
+
+
+const AUTHOR="ABC";
+
+if (AUTHOR){
+    echo "author is ".AUTHOR;
+
+}
+else{
+    echo "author is not set";
+}
+?>
